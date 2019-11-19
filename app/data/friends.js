14ec4b31202736b6fe40016fa17be499fce328d0
Dan Gallagher <dagallagher221@gmail.com>
2019-11-19 12:46:08 -0500

    added friends array data for testing

---
+++ app/data/friends.js
@@ -0,0 +1,11 @@
+//Add initial friend to array so app isnt empty
+
+var friendArray = [
+    {
+        "name": "Lord Farquaad",
+        "photo": "https://www.stickpng.com/assets/images/59f8787f3cec115efb36238b.png",
+        "scores": ["5", "4", "3", "2", "1", "1", "2", "3", "4", "5"]
+    }
+];
+
+module.exports = friendArray;
